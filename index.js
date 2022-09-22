6079089e0f920c2dde84d9fc33a61ff7e502bfd9
Pratt <pratt.paredes@gmail.com>
2022-09-22 12:31:43 -0500

    Installing the email.js script

---
+++ index.js
@@ -0,0 +1,8 @@
+// template_3vuycph
+// service_v0vpc6f
+// bwIWr7XwpIPXx7B-1
+
+function contact(event) {
+    event.preventDefault(),  //Por defecto los forms cuando son submit la pagina se refresca, para evitar eso usamos event.preventDefault()
+    console.log('it worked')
+}
