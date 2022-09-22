e35fc99489401357f38134bf774619a3e1403098
Pratt <pratt.paredes@gmail.com>
2022-09-22 13:33:02 -0500

    contact email js done

--- index.js
+++ index.js
@@ -3,6 +3,24 @@
 // bwIWr7XwpIPXx7B-1
 
 function contact(event) {
-    event.preventDefault(),  //Por defecto los forms cuando son submit la pagina se refresca, para evitar eso usamos event.preventDefault()
-    console.log('it worked')
+  event.preventDefault(); //Por defecto los forms cuando son submit la pagina se refresca, para evitar eso usamos event.preventDefault()
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible"
+  emailjs
+    .sendForm(
+      "service_v0vpc6f",
+      "template_3vuycph",
+      event.target,
+      "bwIWr7XwpIPXx7B-1"
+    )
+    .then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on pratt.paredes@gmail.com"
+        )
+    })
 }
