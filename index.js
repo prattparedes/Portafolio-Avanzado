0683eb58ab52545b575da4b73530579cb93afe98
Pratt <pratt.paredes@gmail.com>
2022-09-23 22:01:53 -0500

    shapes added

--- index.js
+++ index.js
@@ -4,7 +4,21 @@
 
 let isModalOpen = false 
 let contrastToggle = false
+const scaleFactor = 1 / 20
 
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape")  // query selector all returns an array
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    if (isOdd) {
+      shapes[i].style.transform = `translate(${x}px, ${y}px)`
+    }
+    else {
+      shapes[i].style.transform = `translate(${-x}px, ${-y}px)`
+    }   
+}}
 
 function toggleContrast() {
   if (contrastToggle) {
